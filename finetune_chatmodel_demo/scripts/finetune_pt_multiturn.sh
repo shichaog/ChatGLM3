e7ef0dde7abd054f1ca11ed3ddaa27d68924b608
shichaog <shichaog@126.com>
2023-12-25 17:46:54 +0800

    Update finetune_pt_multiturn.sh

--- finetune_chatmodel_demo/scripts/finetune_pt_multiturn.sh
+++ finetune_chatmodel_demo/scripts/finetune_pt_multiturn.sh
@@ -8,15 +8,15 @@ NUM_GPUS=1
 MAX_SEQ_LEN=2048
 DEV_BATCH_SIZE=1
 GRAD_ACCUMULARION_STEPS=16
-MAX_STEP=1000
-SAVE_INTERVAL=500
+MAX_STEP=100
+SAVE_INTERVAL=50
 
 DATESTR=`date +%Y%m%d-%H%M%S`
-RUN_NAME=tool_alpaca_pt
+RUN_NAME=fiction_pt
 
 BASE_MODEL_PATH=THUDM/chatglm3-6b
-DATASET_PATH=formatted_data/tool_alpaca.jsonl
-OUTPUT_DIR=output/${RUN_NAME}-${DATESTR}-${PRE_SEQ_LEN}-${LR}
+DATASET_PATH=/kaggle/input/chatglm3-fine-tune/five_fiction_multi-turn.json
+OUTPUT_DIR=/kaggle/working/output/${RUN_NAME}-${DATESTR}-${PRE_SEQ_LEN}-${LR}
 
 mkdir -p $OUTPUT_DIR
 
